b867c63004e90a13ae105eeeb7dd09e79908d626
Luke Morales <lukemorales@live.com>
2022-08-27 16:15:47 -0300

    build: set up package compilation

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import size from 'rollup-plugin-size';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const name = pkg.main.replace(/\.js$/, '');
+
+const bundle = (config) => ({
+  ...config,
+  input: 'src/index.ts',
+  external: (id) => !/^[./]/.test(id),
+});
+
+export default [
+  bundle({
+    plugins: [esbuild(), size(), terser()],
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+  }),
+];
