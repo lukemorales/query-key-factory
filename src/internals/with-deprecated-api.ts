e9de7c2e8989ed3e0d1fc3420a957b067bd54d31
Luke Morales <lukemorales@live.com>
2022-09-11 01:39:32 -0300

    chore: create proxy to keep deprecated api working for users

---
+++ src/internals/with-deprecated-api.ts
@@ -0,0 +1,40 @@
+import { DeprecatedKey } from './types';
+
+const deprecatedKeys = new Map<DeprecatedKey | symbol, string>([
+  ['default', '_def'],
+  ['toScope', '_def'],
+]);
+
+export const withDeprecatedApi = <T extends Record<string, unknown>>(schema: T) => {
+  const handler: ProxyHandler<T> = {
+    get(target, property, receiver) {
+      const originalValue = Reflect.get(target, property, receiver);
+
+      if (typeof originalValue === 'function' || (typeof originalValue === 'object' && originalValue != null)) {
+        return new Proxy(originalValue, handler);
+      }
+
+      const targetKeyForDeprecatedProperty = deprecatedKeys.get(property);
+
+      if (targetKeyForDeprecatedProperty) {
+        console.warn(
+          `"${String(
+            property,
+          )}" is deprecated and will be removed in the next major version of "@lukemorales/query-key-factory". Please use "${targetKeyForDeprecatedProperty}" instead.`,
+        );
+
+        const targetValue = Reflect.get(target, targetKeyForDeprecatedProperty, receiver);
+
+        if (property === 'toScope') {
+          return () => targetValue;
+        }
+
+        return targetValue;
+      }
+
+      return originalValue;
+    },
+  };
+
+  return new Proxy(schema, handler);
+};
