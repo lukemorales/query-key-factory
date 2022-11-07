f74ec8ed8306650fb6d9296d5982605cfce7534c
Luke Morales <lukemorales@live.com>
2022-11-07 20:53:22 -0300

    fix: repair published types (#32)
    * ci: remove cached build and skip NPM release on GPR forced workflow
    
    * fix: repair exported variable is using interface but cannot be named error

--- src/query-context.types.ts
+++ src/query-context.types.ts
@@ -1,14 +1,20 @@
 export type QueryKey = readonly unknown[];
 
+/**
+ * @internal use QueryFunction from "@tanstack/query-core" instead
+ */
 export type QueryFunction<T = unknown, TQueryKey extends QueryKey = QueryKey> = (
-  context: QueryFunctionContext<TQueryKey>,
+  context?: QueryFunctionContext<TQueryKey>,
 ) => T | Promise<T>;
 
-export interface QueryFunctionContext<TQueryKey extends QueryKey = QueryKey, TPageParam = any> {
+/**
+ * @internal use QueryFunctionContext from "@tanstack/query-core" instead
+ */
+export type QueryFunctionContext<TQueryKey extends QueryKey = QueryKey, TPageParam = any> = {
   queryKey: TQueryKey;
   signal?: AbortSignal;
   pageParam?: TPageParam;
   meta: QueryMeta | undefined;
-}
+};
 
 type QueryMeta = Record<string, unknown>;
